96e765620d30a80560d3fc43bf7267c0098c889c
prodfs <61378776+prodfs@users.noreply.github.com>
2026-01-01 19:39:33 +0530

    Change S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-test-reya-project"
+    bucket = "terraform-s3-test-varun-project"
     key    = "terraform/state/infra.tfstate"
     region = "ap-south-1"
   }
