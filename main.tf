3550292a55927c0e54ebd1ba2c159bbf85baeb3b
prodfs <61378776+prodfs@users.noreply.github.com>
2026-01-01 20:20:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -111,6 +111,24 @@ resource "aws_lb_target_group" "web_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main_vpc.id
+
+  health_check {
+    path = "/"
+    port = "traffic-port"
+  }
+}
+
+# Attach EC2 instances to ALB Target Group
+resource "aws_lb_target_group_attachment" "web_server_az1_attachment" {
+  target_group_arn = aws_lb_target_group.web_target_group.arn
+  target_id        = aws_instance.web_server_az1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "web_server_az2_attachment" {
+  target_group_arn = aws_lb_target_group.web_target_group.arn
+  target_id        = aws_instance.web_server_az2.id
+  port             = 80
 }
 
 # Listener
